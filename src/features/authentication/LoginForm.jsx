4b8f6c4074464e6ae5e745b812aa3e29f780921d
Snehal Karki <57265358+SnehalKarkii@users.noreply.github.com>
2024-01-04 21:16:37 +0545

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from '../../ui/SpinnerMini';
 import { useLogin } from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('snehalkarki@gmail.com');
-  const [password, setPassword] = useState('ssnehal07');
+  const [email, setEmail] = useState('snehalkarki123@gmail.com');
+  const [password, setPassword] = useState('snehal123');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
